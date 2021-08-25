5e2e2b2153819c93295e5b51335fac155afad247
raihanmuhammad <michael.raihan@stockbit.com>
2021-08-25 20:54:10 +0700

    feat: added starter page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import classes from "./App.module.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <h1>Welcome to your giphy</h1>
+      <Router>
+        <Link to="/iron-man" className={classes["link-menu"]}>
+          IRON MAN GIPHY
+        </Link>
+        <Link to="/iron-man" className={classes["link-menu"]}>
+          SEARCH YOUR GIPHY
+        </Link>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
